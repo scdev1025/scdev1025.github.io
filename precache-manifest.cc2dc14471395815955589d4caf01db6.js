aa810033c56e9fe43cca2b14f002d4d3cb572cc6
scdev <scdev1025@gmail.com>
2019-03-21 11:36:08 +0700

    Updates

---
+++ precache-manifest.cc2dc14471395815955589d4caf01db6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "8de76beab535d94d8201",
+    "url": "/static/js/main.b518fc46.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "8de76beab535d94d8201",
+    "url": "/static/css/main.ca81cff9.chunk.css"
+  },
+  {
+    "revision": "177961c030cd72775959ea0409ec5312",
+    "url": "/index.html"
+  }
+];
